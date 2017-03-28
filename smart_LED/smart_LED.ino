63c60c5892538a8f55e8356c20e7d19c8e644a69
hikoyoshi <yenliang@stust.edu.tw>
2017-03-28 22:09:40 +0800

    change mode 'C' state,and add new function FLASE

--- smart_LED/smart_LED.ino
+++ smart_LED/smart_LED.ino
@@ -9,18 +9,18 @@
 #define PIN    6 // PIN number for strip 
 #define CPIN   7 // PIN number for RING 
 
-#define BLUE 21  //藍色燈從 21 ~ 15
-#define ORG  14  //橘色燈從 14 ~ 8
+#define BLUE 21  //藍色燈從 21 ~ 14
+#define ORG  13  //橘色燈從 13 ~ 8
 #define RED   7  //紅色燈從 7 ~ 1
 
 #define COUNTS 21  // 燈條總數 
 #define RCOUNTS 8  // 環形總數
 
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(COUNTS, PIN, NEO_GRB + NEO_KHZ800);
-Adafruit_NeoPixel cpixels = Adafruit_NeoPixel(CCOUNTS, CPIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel cpixels = Adafruit_NeoPixel(RCOUNTS, CPIN, NEO_GRB + NEO_KHZ800);
 
 SoftwareSerial BT(10, 11); //RX:10 TX:11
-int delayval = 800; // delay time
+int delayval = 600; // delay time
 
 void setup() {
 #if defined (__AVR_ATtiny85__)
@@ -48,6 +48,7 @@ void setup() {
 
 void loop() {
   byte cmmd[20];
+  int flag= 0;  
   int insize;    //宣告變數insize為一整數
 
   while (1) {
@@ -58,6 +59,7 @@ void loop() {
       for (int i = 0; i < insize; i++) {
         Serial.print(cmmd[i] = char(BT.read())); //將接收的訊息顯示出來
         Serial.println(" ");
+        flag = 0;
       }//for
     } //if
 
@@ -77,15 +79,24 @@ void loop() {
         Serial.println(cmmd[0]);
         pixels.clear();
         CLEAR_RING_LED();
-        RING_COLORS(1);
+        RING_COLORS(2);
         SMART_STRIP(6);
         delay(delayval);
         break;
 
       case 99:
+        //代碼C，首頁---藍色燈條閃爍完後常亮
         Serial.println(cmmd[0]);
         pixels.clear();
-        SMART_STRIP(6);
+        CLEAR_RING_LED();
+        RING_COLORS(2);
+        if (flag < 1){  // 第一次顯示逐顆亮燈
+          STRIP_FLASH();
+          flag = 1;          
+        }
+          else{
+             SMART_STRIP(14); 
+         }
         delay(delayval);
         break;
 
@@ -123,7 +134,13 @@ void loop() {
         Serial.println(cmmd[0]);
         CLEAR_LED();
         CLEAR_RING_LED();
-        RING_PAIR();
+        if (flag < 1){
+            RING_PAIR();
+            flag = 1;
+        }else{
+          RING_COLORS(2);
+        }
+        
         delay(delayval);
         break;
       default:
@@ -135,7 +152,7 @@ void loop() {
     }
   }//while
 }
-void CLEAR_LED() { // clear LED
+void CLEAR_LED() { // clear STRIP LED
   for (int i = 0; i <= BLUE; i++) {
     pixels.setPixelColor(i, pixels.Color(0, 0, 0, 10)); // Moderately bright green color.
     pixels.show();
@@ -149,10 +166,10 @@ void CLEAR_RING_LED() { // clear ring LED
   }
 }
 
-void SMART_STRIP(int num) {
+void SMART_STRIP(uint8_t num) {
   if (num < BLUE && num > ORG ) {
     for (int i = BLUE; i >= num; i--) {
-      pixels.setPixelColor(i, pixels.Color(0, 0, 8)); // setting 21~15 Led color to blue
+      pixels.setPixelColor(i, pixels.Color(0, 25, 8)); // setting 21~15 Led color to blue
       pixels.show();
     }
   } else if (num == ORG) {
@@ -178,7 +195,7 @@ void SMART_STRIP(int num) {
   }
 }
 
-void RING_COLORS(int state) {
+void RING_COLORS(uint8_t state) {
   // 0 是橘色，1是紅色，2是綠色
   if (state == 0) {
     for (int i = 0; i < RCOUNTS; i++) {
@@ -198,12 +215,26 @@ void RING_COLORS(int state) {
   }
 }
 
+void STRIP_FLASH() {  // 進入首頁 -- 第一次閃爍
+  for(int x=0;x<1;x++){
+    for (int i = BLUE; i >= 14; i--) {
+      pixels.setPixelColor(i, pixels.Color(0, 25, 8)); // setting strip Led color to blue.
+      pixels.show();
+      delay(400);
+    }
+    CLEAR_LED();
+  }  
+}
+
 void RING_PAIR() {  // 持續亮綠色  -- 配對成功
-  for (int i = 0; i < RCOUNTS; i++) {
-    cpixels.setPixelColor(i, cpixels.Color(0, 10, 0)); // setting ring Led color to green.
-    cpixels.show();
-  }
-  delay(200);
+  for(int x=0;x<3;x++){
+    for (int i = 0; i < RCOUNTS; i++) {
+      cpixels.setPixelColor(i, cpixels.Color(0, 10, 0)); // setting ring Led color to green.
+      cpixels.show();
+      delay(200);
+    }
+    CLEAR_RING_LED();
+  }  
 }
 
 
